93c6af58b21a2d28450706d9c18a300c36674bd5
xiaoz <xiaoz93@outlook.com>
2025-01-08 14:48:57 +0800

    add tcpmk_node

---
+++ tcpmk_node/install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+BASE_DIR="/opt/tcpmk_node"
+
+# 初始化环境
+init() {
+    #更新软件
+    apk update
+    #安装timezone
+    apk add -U tzdata
+    #安装必要软件
+    apk add curl wget
+    #查看时区列表
+    ls /usr/share/zoneinfo
+    #拷贝需要的时区文件到localtime
+    cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+    #查看当前时间
+    date
+    #为了精简镜像，可以将tzdata删除了
+    apk del tzdata
+    #拷贝运行文件
+    chmod +x /root/run.sh
+    cp /root/run.sh /usr/sbin/
+}
+
+# 下载包
+install() {
+    cd ${BASE_DIR}
+    wget https://soft.xiaoz.org/UniBin/tcpmk_node/tcpmk_node.tar.gz
+    tar -zxvf tcpmk_node.tar.gz
+    rm -rf tcpmk_node.tar.gz
+    chmod +x tcpmk_node
+}
+
+init
---
+++ tcpmk_node/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# 运行zdir
+run() {
+    cd /opt/tcpmk_node
+    chmod +x sh/*.sh
+    # 设置DNS
+    # echo "nameserver 223.5.5.5" > /etc/resolv.conf
+    # echo "nameserver 119.29.29.29" >> /etc/resolv.conf
+    # 后台运行Zdir
+    ./tcpmk_node start
+    # 判断架构
+    # get_arch=$(arch)
+    # if [[ "${get_arch}" == "x86_64" ]]
+    # then
+    #     ./zdir start
+    # elif [[ "${get_arch}" == "aarch64" ]]
+    # then
+    #     ./zdir_arm64 start
+    # else
+    #     ./zdir_arm start
+    # fi
+}
+
+run
