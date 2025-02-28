3ed7983b919aa244cb49275dea009f126e170d4a
xiaoz <xiaoz93@outlook.com>
2025-02-28 17:21:05 +0800

    change os

--- wp2ai/install.sh
+++ wp2ai/install.sh
@@ -4,21 +4,13 @@ BASE_DIR="/opt/wp2ai"
 
 # 初始化环境
 init() {
-    #更新软件
-    apk update
-    #安装timezone
-    apk add -U tzdata
-    #安装必要软件
-    apk add curl wget
-    #查看时区列表
-    ls /usr/share/zoneinfo
-    #拷贝需要的时区文件到localtime
-    cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-    #查看当前时间
-    date
-    #为了精简镜像，可以将tzdata删除了
-    apk del tzdata
-    #拷贝运行文件
+    # 更新软件
+    apt-get update
+
+    # 安装必要软件
+    apt-get install -y curl wget build-essential && rm -rf /var/lib/apt/lists/*
+
+    # 拷贝运行文件
     chmod +x /root/run.sh
     cp /root/run.sh /usr/sbin/
 }
