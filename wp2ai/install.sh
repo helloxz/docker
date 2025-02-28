bf1467496553fbcb5b3782615d044ac98964672f
xiaoz <xiaoz93@outlook.com>
2025-02-28 17:34:27 +0800

    fix bug

--- wp2ai/install.sh
+++ wp2ai/install.sh
@@ -4,10 +4,12 @@ BASE_DIR="/opt/wp2ai"
 
 # 初始化环境
 init() {
-    # 更新软件
-    apt-get update
     # 导入公钥
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9 6ED0E7B82643E131 F8D2585B8783D481 54404762BBB6E853 BDE6D2B9216EC7A8
+    
+    # 更新软件
+    apt-get update
+    
     # 安装必要软件
     apt-get install -y curl wget build-essential && rm -rf /var/lib/apt/lists/*
 
